39caaa9f3ca774ae6f8db0fd3e5c56163a3fbc62
Chigozie Nri <cnri@enthought.com>
2021-07-20 20:10:33 +0100

    Black, isort, flake8

--- src/bmp2sysex/__main__.py
+++ src/bmp2sysex/__main__.py
@@ -1,28 +1,30 @@
-#!/usr/bin/env python 
-from PIL import Image
-import numpy as np
-import sys
-import argparse 
+#!/usr/bin/env python
+import argparse
 import warnings
+
+import numpy as np
+from PIL import Image
+
 
 def main(path, white1=False):
     im = Image.open(path)
-    if im.mode != '1':
-        warnings.warn('Only 1-bit images are supported. Will convert to 1-bit')
-    im = im.convert('1')
+    if im.mode != "1":
+        warnings.warn("Only 1-bit images are supported. Will convert to 1-bit")
+    im = im.convert("1")
     if im.size != (16, 16):
         im.close()
-        raise RuntimeError('Only 16x16 images are supported')
+        raise RuntimeError("Only 16x16 images are supported")
     arr = np.asarray(im).astype(np.int32)
     # White is 0, black is 1 => invert compared to PIL default
     if not white1:
         arr = 1 - arr
-    string = ''.join(arr.flatten().astype('str').tolist())
-    return(string)
+    string = "".join(arr.flatten().astype("str").tolist())
+    return string
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Convert bitmap to SysEx')
-    parser.add_argument('path', type=str, help='path to bitmap image')
-    parser.add_argument('-w', '--white1', help='Interpret 1 as white')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert bitmap to SysEx")
+    parser.add_argument("path", type=str, help="path to bitmap image")
+    parser.add_argument("-w", "--white1", help="Interpret 1 as white")
     args = parser.parse_args()
     print(main(args.path, white1=args.white1))
--- tests/test_bmp2sysex.py
+++ tests/test_bmp2sysex.py
@@ -1,60 +1,86 @@
-from bmp2sysex.__main__ import main
-from unittest import TestCase
-import numpy as np
-from PIL import Image
 import tempfile
 import warnings
+from unittest import TestCase
+
+import numpy as np
+from PIL import Image
+
+from bmp2sysex.__main__ import main
+
 
 class TestMain(TestCase):
-
     def test_16x16_1bit_png(self):
-        expected = '1010100000000001101010000000000011101000000000011010100000000000101010000000000100000000000000000000000000000001110101010111000010010101010100011001110101110000100101010101000111010101010100000000000000000001000000000000000000000000000000011010101010101010'
-        arr = np.array([1 - int(i) for i in expected]).reshape((16, 16)).astype(np.bool)
-        with tempfile.NamedTemporaryFile(suffix='.png') as tmp:
+        expected = "1010100000000001101010000000000011101000000000011010100000000000101010000000000100000000000000000000000000000001110101010111000010010101010100011001110101110000100101010101000111010101010100000000000000000001000000000000000000000000000000011010101010101010"  # noqa: E501
+        arr = (
+            np.array([1 - int(i) for i in expected])
+            .reshape((16, 16))
+            .astype(np.bool)
+        )
+        with tempfile.NamedTemporaryFile(suffix=".png") as tmp:
             im = Image.fromarray(arr)
             im.mode = "1"
             im.save(tmp.name)
             self.assertEqual(main(tmp.name), expected)
 
     def test_16x16_1bit_bmp(self):
-        expected = '1010100000000001101010000000000011101000000000011010100000000000101010000000000100000000000000000000000000000001110101010111000010010101010100011001110101110000100101010101000111010101010100000000000000000001000000000000000000000000000000011010101010101010'
-        arr = np.array([1 - int(i) for i in expected]).reshape((16, 16)).astype(np.bool)
-        with tempfile.NamedTemporaryFile(suffix='.bmp') as tmp:
+        expected = "1010100000000001101010000000000011101000000000011010100000000000101010000000000100000000000000000000000000000001110101010111000010010101010100011001110101110000100101010101000111010101010100000000000000000001000000000000000000000000000000011010101010101010"  # noqa: E501
+        arr = (
+            np.array([1 - int(i) for i in expected])
+            .reshape((16, 16))
+            .astype(np.bool)
+        )
+        with tempfile.NamedTemporaryFile(suffix=".bmp") as tmp:
             im = Image.fromarray(arr)
             im.mode = "1"
             im.save(tmp.name)
             self.assertEqual(main(tmp.name), expected)
 
     def test_16x16_8bit(self):
-        expected = '1010100000000001101010000000000011101000000000011010100000000000101010000000000100000000000000000000000000000001110101010111000010010101010100011001110101110000100101010101000111010101010100000000000000000001000000000000000000000000000000011010101010101010'
-        arr = np.array([255 * (1 - int(i)) for i in expected]).reshape((16, 16)).astype(np.bool)
-        with tempfile.NamedTemporaryFile(suffix='.png') as tmp:
+        expected = "1010100000000001101010000000000011101000000000011010100000000000101010000000000100000000000000000000000000000001110101010111000010010101010100011001110101110000100101010101000111010101010100000000000000000001000000000000000000000000000000011010101010101010"  # noqa: E501
+        arr = (
+            np.array([255 * (1 - int(i)) for i in expected])
+            .reshape((16, 16))
+            .astype(np.bool)
+        )
+        with tempfile.NamedTemporaryFile(suffix=".png") as tmp:
             im = Image.fromarray(arr)
             im.mode = "L"
             im.save(tmp.name)
             with warnings.catch_warnings(record=True) as warning_list:
                 main(tmp.name)
             self.assertIn(
-                'Only 1-bit images are supported. Will convert to 1-bit',
-                [str(warning.message) for warning in warning_list]
+                "Only 1-bit images are supported. Will convert to 1-bit",
+                [str(warning.message) for warning in warning_list],
             )
 
     def test_8x8_1bit_png(self):
-        expected = '1010100000000001101010000000000011101000000000011010100000000000'
-        arr = np.array([1 - int(i) for i in expected]).reshape((8, 8)).astype(np.bool)
-        with tempfile.NamedTemporaryFile(suffix='.png') as tmp:
+        expected = (
+            "1010100000000001101010000000000011101000000000011010100000000000"
+        )
+        arr = (
+            np.array([1 - int(i) for i in expected])
+            .reshape((8, 8))
+            .astype(np.bool)
+        )
+        with tempfile.NamedTemporaryFile(suffix=".png") as tmp:
             im = Image.fromarray(arr)
             im.mode = "1"
             im.save(tmp.name)
             with self.assertRaises(RuntimeError) as err:
                 main(tmp.name)
-            self.assertIn('Only 16x16 images are supported', str(err.exception))
+            self.assertIn(
+                "Only 16x16 images are supported", str(err.exception)
+            )
 
     def test_white1(self):
-        expected = '1010100000000001101010000000000011101000000000011010100000000000101010000000000100000000000000000000000000000001110101010111000010010101010100011001110101110000100101010101000111010101010100000000000000000001000000000000000000000000000000011010101010101010'
-        arr = np.array([int(i) for i in expected]).reshape((16, 16)).astype(np.bool)
-        with tempfile.NamedTemporaryFile(suffix='.png') as tmp:
+        expected = "1010100000000001101010000000000011101000000000011010100000000000101010000000000100000000000000000000000000000001110101010111000010010101010100011001110101110000100101010101000111010101010100000000000000000001000000000000000000000000000000011010101010101010"  # noqa: E501
+        arr = (
+            np.array([int(i) for i in expected])
+            .reshape((16, 16))
+            .astype(np.bool)
+        )
+        with tempfile.NamedTemporaryFile(suffix=".png") as tmp:
             im = Image.fromarray(arr)
             im.mode = "1"
             im.save(tmp.name)
-            self.assertEqual(main(tmp.name, white1=True), expected) 
+            self.assertEqual(main(tmp.name, white1=True), expected)
